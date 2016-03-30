a2fd0f0d933e043e65e5b8dd069a44be9fd068cc
teguh6706144027 <guhz_123@yahoo.com>
2016-03-30 10:22:00 +0700

    Update JurnalKodingan.java

--- JurnalKodingan.java
+++ JurnalKodingan.java
@@ -1,48 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package jurnal.kodingan;
 
-/**
- *
- * @author Praktikan
- */
+
 import java.util.Scanner;
 
 public class JurnalKodingan {
 
-    /**
-     * @param args the command line arguments
-     */
     
     public static int fibonacci(int n){
         
        
-        if (n<=2){
-            
-            //System.out.print("1, ");
+        if (n<=2)
             return 1;
-            
-            
-        }else{
-            
-            
+        else
             return (fibonacci(n-1) + fibonacci(n-2));
-            
-        }
         
     }
     
     public static void tampilFibonacci(int n){
         
-         for (int i = 1; i<=n; i++){
-            
+         for (int i = 1; i<=n; i++)
             System.out.print(fibonacci(i) + ", ");
             
-        }
-        
     }
     
     public static void main(String[] args) {
