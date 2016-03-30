21fdcc6cdc9e9c40f8a5ab77ab04c93c931b97fa
teguh6706144027 <guhz_123@yahoo.com>
2016-03-30 10:30:13 +0700

    Update JurnalKodingan.java

--- JurnalKodingan.java
+++ JurnalKodingan.java
@@ -1,10 +1,18 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package jurnal.kodingan;
 
-
+/**
+ *
+ * @author Praktikan
+ */
 import java.util.Scanner;
 
-public class JurnalKodingan {
+public class SumOfFibonacci {
+
 
     
     public static int fibonacci(int n){
@@ -12,16 +20,24 @@ public class JurnalKodingan {
        
         if (n<=2)
             return 1;
-        else
+        else    
             return (fibonacci(n-1) + fibonacci(n-2));
+          
         
     }
     
-    public static void tampilFibonacci(int n){
+    public static int tampilFibonacci(int n){
         
-         for (int i = 1; i<=n; i++)
-            System.out.print(fibonacci(i) + ", ");
+        int total = 0;
+         for (int i = 1; i<=n; i++){
+             
+           System.out.print(fibonacci(i) + ", ");
+           total += fibonacci(i);
+             
+         }
             
+            
+        return total;
     }
     
     public static void main(String[] args) {
@@ -32,7 +48,9 @@ public class JurnalKodingan {
         System.out.print("Masukkan bilangan : ");
         bil = input.nextInt();
         
-       tampilFibonacci(7);
+       int tot = tampilFibonacci(7);
+       System.out.println("");
+        System.out.println("Total-nya Adalah : " + tot);
         
     }
     
